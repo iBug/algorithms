a1824eb6814dc32b3a62aa8c2902c954e7c11d30
iBug <git@ibugone.com>
2019-10-10 01:17:31 +0800

    Fix for data ranges

--- inverse-pairs/main.c
+++ inverse-pairs/main.c
@@ -2,12 +2,13 @@
 #include <stdlib.h>
 
 #ifndef MOD
-#define MOD 1000013
+#define MOD 10000019UL
 #endif
 
-int merge(int* a, int l, int m, int r) {
+unsigned long merge(int* a, int l, int m, int r) {
     int *b = malloc((r - l) * sizeof(int));
-    int i = 0, j = l, k = m, s = 0;
+    int i = 0, j = l, k = m;
+    unsigned long s = 0;
     while (j < m && k < r) {
         if (a[j] <= a[k]) {
             b[i] = a[j];
@@ -29,10 +30,11 @@ int merge(int* a, int l, int m, int r) {
     return s;
 }
 
-int merge_sort(int* a, int l, int r) {
+unsigned long merge_sort(int* a, int l, int r) {
     if (l + 1 >= r)
         return 0;
-    int s = 0, m = (l + r) / 2;
+    int m = (l + r) / 2;
+    unsigned long s = 0;
     s = (s + merge_sort(a, l, m)) % MOD;
     s = (s + merge_sort(a, m, r)) % MOD;
     s = (s + merge(a, l, m, r)) % MOD;
@@ -45,8 +47,8 @@ int main() {
     a = malloc(n * sizeof(int));
     for (int i = 0; i < n; i++)
         scanf("%d", &a[i]);
-    int s = merge_sort(a, 0, n);
+    unsigned long s = merge_sort(a, 0, n);
     free(a);
-    printf("%d\n", s);
+    printf("%lu\n", s);
     return 0;
 }
