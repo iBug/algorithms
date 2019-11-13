d36eec86184d6c080ef65c2b7a82ce46380faf43
iBug <git@ibugone.com>
2019-11-14 02:49:48 +0800

    More code

--- rb-tree/rb-tree.hpp
+++ rb-tree/rb-tree.hpp
@@ -5,9 +5,10 @@
 #error "C++11 required"
 #endif // Version check
 
+#include <stack>
+
 namespace iBug {
-    enum RBColorT {RB_BLACK = 0, RB_RED = 1};
-    using RBColor = enum RBColorT;
+    typedef enum RBColor {RB_BLACK = 0, RB_RED = 1} RBColor;
 
     template <typename T>
     class RBTree {
@@ -23,7 +24,7 @@ namespace iBug {
                 return reinterpret_cast<const char*>(this) ==
                        reinterpret_cast<const char*>(&other);
             }
-            bool operator!=(const Node& other) const { return !(*this == other); }
+            inline bool operator!=(const Node& other) const { return !(*this == other); }
         };
 
         private:
@@ -35,20 +36,47 @@ namespace iBug {
         RBTree(const RBTree<T>&) = delete;
         RBTree& operator=(const RBTree<T>&) = delete;
 
-        RBTree& leftRotate(Node& x);
-        RBTree& rightRotate(Node& x);
-        RBTree& insert(Node& x);
+        Node& minimum(const Node& x);
+        Node& minimum() { return minimum(this->root); };
+
+        RBTree& leftRotate(Node&);
+        RBTree& rightRotate(Node&);
+        RBTree& insert(Node&);
         RBTree& insert(const T& value) { return insert(*(new Node(value))); }
-        void insertFixup(Node& x);
+        void insertFixup(Node&);
+        RBTree& transplant(Node&, Node&);
+        RBTree& pop(Node&);
+        void deleteFixup(Node&);
     };
 
     template <typename T> RBTree<T>::RBTree(void) {
         nil = new Node;
         nil->color = RB_BLACK;
+        root = nil;
     }
 
     template <typename T> RBTree<T>::~RBTree() {
+        std::stack<Node*> stack;
+        if (root != nil)
+            stack.push(root);
+        while (!stack.empty()) {
+            auto *top = stack.pop();
+            if (top->left != nil)
+                stack.push(top->left);
+            if (top->right != nil)
+                stack.push(top->right);
+            delete top;
+        }
         delete nil;
+    }
+
+    template <typename T> typename RBTree<T>::Node& RBTree<T>::minimum(const Node& x) {
+        if (&x == nil)
+            return *nil;
+        auto *px = &x;
+        while (px->left != nil)
+            px = px->left;
+        return *px;
     }
 
     template <typename T> RBTree<T>& RBTree<T>::leftRotate(Node& x) {
@@ -109,6 +137,139 @@ namespace iBug {
     }
 
     template <typename T> void RBTree<T>::insertFixup(Node& z) {
+        auto *pz = &z;
+        while (pz->p->color == RB_RED) {
+            if (pz->p == pz->p->p->left) {
+                auto &y = pz->p->p->right;
+                if (y.color == RB_RED) {
+                    pz->p->color = RB_BLACK;
+                    y.color = RB_BLACK;
+                    pz->p->p->color = RB_RED;
+                    pz = pz->p->p;
+                } else {
+                    if (pz == pz->p->right) {
+                        pz = pz->p;
+                        leftRotate(*pz);
+                    }
+                    pz->p->color = RB_BLACK;
+                    pz->p->p->color = RB_RED;
+                    rightRotate(*pz->p->p);
+                }
+            } else {
+                auto &y = pz->p->p->left;
+                if (y.color == RB_RED) {
+                    pz->p->color = RB_BLACK;
+                    y.color = RB_BLACK;
+                    pz->p->p->color = RB_RED;
+                    pz = pz->p->p;
+                } else {
+                    if (pz == pz->p->left) {
+                        pz = pz->p;
+                        rightRotate(*pz);
+                    }
+                    pz->p->color = RB_BLACK;
+                    pz->p->p->color = RB_RED;
+                    leftRotate(*pz->p->p);
+                }
+            }
+            root->color = RB_BLACK;
+        }
+    }
+
+    template <typename T> RBTree<T>& RBTree<T>::transplant(Node& u, Node& v) {
+        if (*u.p == *nil)
+            root = v;
+        else if (u == *u.p->left)
+            u.p->left = v;
+        else
+            u.p->right = v;
+        v.p = u.p;
+    }
+
+    template <typename T> RBTree<T>& RBTree<T>::pop(Node& z) {
+        auto y_original_color = z.color;
+        auto *px = &z;
+        if (z.left == nil) {
+            px = z.right;
+            transplant(z, *z.right);
+        } else if (z.right == nil) {
+            px = z.left;
+            transplant(z, *z.left);
+        } else {
+            auto &y = minimum(z);
+            y_original_color = y.color;
+            auto &x = *y.right;
+            if (y.p == &z)
+                x.p = &y;
+            else {
+                transplant(y, *y.right);
+                y.right = z.right;
+                y.right->p = &y;
+            }
+            transplant(z, y);
+            y.left = z.left;
+            y.left->p = &y;
+            y.color = z.color;
+        }
+        if (y_original_color == RB_BLACK)
+            deleteFixup(*px);
+        return *this;
+    }
+
+    template <typename T> void RBTree<T>::deleteFixup(Node& x) {
+        auto *px = &x;
+        while (px != root && px->color == RB_BLACK) {
+            if (px == px->p->left) {
+                auto *pw = px->p->right;
+                if (pw->color == RB_RED) {
+                    pw->color = RB_BLACK;
+                    px->p->color = RB_RED;
+                    leftRotate(*px->p);
+                    pw = px->p->right;
+                }
+                if (pw->left->color == RB_BLACK && pw->right->color == RB_BLACK) {
+                    pw->color = RB_RED;
+                    px = px->p;
+                } else {
+                    if (pw->right->color == RB_BLACK) {
+                        pw->left->color = RB_BLACK;
+                        pw->color = RB_RED;
+                        rightRotate(*pw);
+                        pw = px->p->right;
+                    }
+                    pw->color = px->p->color;
+                    px->p->color = RB_BLACK;
+                    pw->right->color = RB_BLACK;
+                    leftRotate(*px->p);
+                    px = root;
+                }
+            } else {
+                auto *pw = px->p->left;
+                if (pw->color == RB_RED) {
+                    pw->color = RB_BLACK;
+                    px->p->color = RB_RED;
+                    rightRotate(*px->p);
+                    pw = px->p->left;
+                }
+                if (pw->right->color == RB_BLACK && pw->left->color == RB_BLACK) {
+                    pw->color = RB_RED;
+                    px = px->p;
+                } else {
+                    if (pw->left->color == RB_BLACK) {
+                        pw->right->color = RB_BLACK;
+                        pw->color = RB_RED;
+                        leftRotate(*pw);
+                        pw = px->p->left;
+                    }
+                    pw->color = px->p->color;
+                    px->p->color = RB_BLACK;
+                    pw->left->color = RB_BLACK;
+                    rightRotate(*px->p);
+                    px = root;
+                }
+            }
+        }
+        px->color = RB_BLACK;
     }
 }
 
