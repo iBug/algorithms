05645a65e984cbab69fa4768a0af0378bd5f0327
iBug <git@ibugone.com>
2019-12-06 14:53:46 +0800

    Add two-way BFS

--- bfs-csr/dual-way.cpp
+++ bfs-csr/dual-way.cpp
@@ -41,6 +41,7 @@ int main() {
     fin.close();
 
     int source, target;
+    //cout << "Enter source and target: ";
     cin >> source >> target;
     if (source < 0 || source >= NV || target < 0 || target >= NV) {
         cerr << "Invalid node!" << endl;
@@ -48,27 +49,53 @@ int main() {
     }
 
     deque<int> q;
-    vector<bool> visited(NV, false);
+    deque<int> modes; // 0 - forward, 1 - backward
     vector<int> pred(NV, -1);
+    vector<int> next(NV, -1);
+    pred[source] = source;
+    next[target] = target;
 
+    int intersect = -1;
     q.push_back(source);
+    q.push_back(target);
+    modes.push_back(0);
+    modes.push_back(1);
     while (!q.empty()) {
         auto node = q.front();
+        auto mode = modes.front();
         q.pop_front();
+        modes.pop_front();
 
         // Lookup neighbours of [node]
         for (int i = IA[node]; i < IA[node + 1]; i++) {
-            auto next = JA[i];
-            if (visited[next])
-                continue;
-            pred[next] = node;
-            visited[next] = true;
-            q.push_back(next);
+            auto &n = JA[i];
+            if (mode == 0) {
+                if (pred[n] != -1)
+                    continue;
+                pred[n] = node;
+            } else {
+                if (next[n] != -1)
+                    continue;
+                next[n] = node;
+            }
+            if (pred[n] != -1 && next[n] != -1) {
+                intersect = n;
+                break;
+            }
+            q.push_back(n);
+            modes.push_back(mode);
         }
+        if (intersect != -1)
+            break;
     }
 
-    for (auto node = target; node != source; node = pred[node])
+    q.clear();
+    int asd = 0;
+    for (auto node = intersect; node != source; node = pred[node])
         q.push_back(node);
+    for (auto node = next[intersect]; node != target; node = next[node])
+        q.push_front(node);
+    q.push_front(target);
     cout << source;
     while (!q.empty()) {
         cout << " -> " << q.back();
